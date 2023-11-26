001ba7ab2bd9412c0b6cff3126115557bd2efae7
Dima <perebyakin_dmitr@mail.ru>
2023-11-26 15:06:18 +0300

    solution from seminar 3

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,37 @@
+﻿// double findDistance (int x1, int y1, int x2, int y2)
+// {
+//     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2));
+// }
+
+// int x1 = int.Parse(Console.ReadLine() ?? "0");
+// int y1 = int.Parse(Console.ReadLine() ?? "0");
+// int x2 = int.Parse(Console.ReadLine() ?? "0");
+// int y2 = int.Parse(Console.ReadLine() ?? "0");
+
+// Console.WriteLine(findDistance(x1, y1, x2, y2));
+
+
+
+
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+// double Distance (int x, int y)
+// {
+//     double distance = Math.Sqrt(x*x+y*y);
+//     return distance;
+// }
+// void PrintResult(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// int x1 = ReadData("Введите координаты x1: ");
+// int y1 = ReadData("Введите координаты y1: ");
+// int x2 = ReadData("Введите координаты x2: ");
+// int y2 = ReadData("Введите координаты y2: ");
+// double res = Distance((x2-x1),(y2-y1));
+// PrintResult("Расстояние равно " + $"{res}");
---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,27 @@
+﻿// int ReadData(string msg)
+//  {   
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+//  }
+
+//  string BuildLine(int n, int pow)
+//  {
+//     string res = string.Empty; (или string res = " ";)
+//     for(int i = 1; i <= n; i++)
+//     {
+//         res = res + Math.Pow(i,pow)+ " ";
+//     }
+//     return res;
+//  }
+
+// void PrintResult(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+
+// int n = ReadData("Введите число");
+// PrintResult(BuildLine(n, 1));
+// PrintResult(BuildLine(n, 2));
+
+
